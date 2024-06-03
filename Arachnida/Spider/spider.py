9d7d074b011eff2897744028e2cf54b9ec552073
MaddRaven <faivrefx@gmail.com>
2024-06-03 14:59:09 +0200

    Spiderman Spiderman

--- Arachnida/Spider/spider.py
+++ Arachnida/Spider/spider.py
@@ -1,4 +1,47 @@
-import argparse
+#!/usr/bin/env python3
+
+import os, argparse, requests, re
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+
+def get_imgs(url):
+    response = requests.get(url)
+
+    imgs_url = []
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "lxml")
+        imgs = soup.find_all('img')
+
+        for img in imgs:
+            img_url = img.attrs.get('src')
+            if not img_url.startswith('http'):
+                img_url = urljoin(url, img_url)
+            imgs_url.append(img_url)
+    
+    return imgs_url
+
+
+def download_imgs(imgs_url, path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    for i, url in enumerate(imgs_url):
+        try:
+            response = requests.get(url)
+
+            extension = url.split('.')[-1].lower()
+            if (extension in ['jpg', 'jpeg', 'png', 'gif', 'bmp']):
+                filename = f"{path}/image_{i}.{extension}"
+                with open(filename, 'wb') as file:
+                    file.write(response.content)
+                print(f"Download OK: {filename}")
+            else:
+                print(f"Download KO: {path}/image_{i}:{extension}")
+
+        except Exception as e:
+            print(f"Error while downloading {url}: {str(e)}")
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser("Spider program to download images")
@@ -8,12 +51,15 @@ def parse_arguments():
     parser.add_argument('URL', help="URL targeted")
     return parser.parse_args()
 
+
 def main():
     args = parse_arguments()
-    print(args.recursive)
-    print(args.level)
-    print(args.path)
-    print(args.URL)
+    url = args.URL
+    path = args.path
+    level = args.level
+
+    imgs_url = get_imgs(url)
+    download_imgs(imgs_url, path)
 
 
 if __name__ == "__main__":
