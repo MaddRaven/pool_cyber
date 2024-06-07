c914955f348e64c1aa0ddf0fb11cc1aa7f252841
MaddRaven <faivrefx@gmail.com>
2024-06-07 09:40:02 +0200

    Auto-Commit

--- ft_onion/entrypoint.sh
+++ ft_onion/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo "Starting Tor..."
 /usr/sbin/tor -f /etc/tor/torrc
 sleep 5
+
+echo "Starting SSHD..."
 /usr/sbin/sshd -D -e -p 4343
+
+echo "Starting Nginx..."
 nginx -g 'daemon off;'
+
+echo "Container ready."
 tail -f /dev/null
